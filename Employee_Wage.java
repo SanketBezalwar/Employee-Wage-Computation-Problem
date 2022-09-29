97fd3b6dbd7bc36c5e9e8410423781755faee82f
SanketBezalwar <111625762+SanketBezalwar@users.noreply.github.com>
2022-09-30 00:16:54 +0530

    Update Employee_Wage.java

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,10 +1,11 @@
 package Employee_Wage_Computation;
-import java.util.Random;
 
+import java.util.Random;
 
 public class Employee_Wage {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random inp = new Random();
 		int isPresent = inp.nextInt(2);
@@ -15,5 +16,4 @@ public class Employee_Wage {
 			System.out.println("Employee is Absent");
 		}
 	}
-
 }
