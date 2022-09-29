0afd8d0d2d70e7ffb2fb2d8d1bd2085949bedab6
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-09-29 23:38:40 +0530

    Updated Attendance

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,9 +1,19 @@
 package Employee_Wage_Computation;
+import java.util.Random;
+
 
 public class Employee_Wage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random inp = new Random();
+		int isPresent = inp.nextInt(2);
+		if (isPresent == 1) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
