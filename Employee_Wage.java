f4cfb661e9e41e2855634095b64ebaa65c1eb0ef
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-09-30 15:00:32 +0530

    Solved using switch case

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -10,11 +10,11 @@ public class Employee_Wage {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("Welcome to Employee Wage Computation");
 		Random inp = new Random();
 		isPresent = inp.nextInt(2);
-
-		if (isPresent == 1) {
+		
+		if (isPresent == 0) {
 			System.out.println("Employee is Present");
 		} else {
 			System.out.println("Employee is Absent");
@@ -23,10 +23,31 @@ public class Employee_Wage {
 		// Calculate daily employee wage
 
 		int dailyWage = 0;
-		dailyWage = full_Day_Hour * wage_Per_Hour;
-		dailyWage = part_Time_Hour * wage_Per_Hour;
 
+		dailyWage = full_Day_Hour * wage_Per_Hour;
 		System.out.println("Daily wage of Employee is " + dailyWage);
+
+		dailyWage = part_Time_Hour * 10;
 		System.out.println("Employee is Part Time");
+		System.out.println("Daily wage of part time employee is " + dailyWage + " Rs");
+
+		
+		isPresent = inp.nextInt(3);
+		switch (isPresent) {
+		case 0:
+			System.out.println("Employee is Present");
+			break;
+
+		case 1:
+			System.out.println("Employee is Part Time");
+			break;
+
+		case 2:
+			System.out.println("Employee is Absent");
+			break;
+		
+
+		}
+
 	}
 }
