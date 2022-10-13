81586e71c5843bcf49ff1e6f7b2dc9f6269549b7
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-13 16:02:53 +0530

    Refactor the code

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -8,83 +8,82 @@ public class Employee_Wage {
 	static int full_Day_Hour = 8;
 	static int part_Time_Hour = 8;
 
-	public static void main(String[] args) {
+	// calculate employee wage
+	public void dailyEmployeeWage() {
+		int dailyWage1;
+		int dailyWage2;
+		int monthlyWage1;
+		int monthlyWage2;
+		int workDays = 20;
 
-		System.out.println("Welcome to Employee Wage Computation");
-		System.out.println();
+		dailyWage1 = full_Day_Hour * wage_Per_Hour;
+		dailyWage2 = part_Time_Hour * wage_Per_Hour;
+
+		monthlyWage1 = dailyWage1 * workDays;
+		monthlyWage2 = dailyWage2 * workDays;
+
+		System.out.println("Calculate Employee Wage : ");
+
 		Random inp = new Random();
-		isPresent = inp.nextInt(2);
-
-		if (isPresent == 0) {
-			System.out.println("Employee is Present");
-		} else {
-			System.out.println("Employee is Absent");
-		}
-
-		// Calculate daily employee wage
-
-		int dailyWage = 0;
-		int dailyWage2 = 0;
-
-		dailyWage = full_Day_Hour * wage_Per_Hour;
-		System.out.println("Daily wage of Employee is " + dailyWage);
-
-		dailyWage2 = part_Time_Hour * 10;
-		System.out.println("Employee is Part Time");
-		System.out.println("Daily wage of part time employee is " + dailyWage2 + " Rs");
-
-		// Solved using switch case
-
 		isPresent = inp.nextInt(3);
 		switch (isPresent) {
 		case 0:
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Full Time");
+			System.out.println("Daily Wage of Employee is " + dailyWage1 + " Rs ");
+			System.out.println("Monthly Wage of Employee is " + monthlyWage1 + " Rs ");
 			break;
 
 		case 1:
 			System.out.println("Employee is Part Time");
+			System.out.println("Daily Wage of Employee is " + dailyWage2 + " Rs ");
+			System.out.println("Monthly Wage of Employee is " + monthlyWage2 + " Rs ");
 			break;
 
 		case 2:
 			System.out.println("Employee is Absent");
 			break;
 		}
+	}
 
-		// Calculate monthly wage
-
-		int monthlyWage = 0;
-		int monthDays = 20;
-
-		monthlyWage = dailyWage * monthDays;
-		System.out.println("Monthly wage of Employee is " + monthlyWage);
-
-		monthlyWage = dailyWage2 * monthDays;
-		System.out.println("Monthly wage of part time Employee is " + monthlyWage);
-
-		// Wages till hours or days is reached
-
+	// wages till hours or days is reached
+	public void wageDaysHours() {
 		int isWorking;
-		int monthlyWage2 = 0;
-		int workingDays = 0;
-		int workingHours = 0;
+		int totalWage = 0;
+		int maxWorkDays = 0;
+		int maxWorkHours = 0;
 
-		while (workingHours <= 100 && workingDays <= 20) {
+		while (maxWorkHours <= 100 && maxWorkDays <= 20) {
+
+			Random inp = new Random();
 			isWorking = inp.nextInt(2);
-			workingDays = workingDays + 1;
+			maxWorkDays += 1;
 
 			switch (isWorking) {
 			case 0:
-				monthlyWage2 = monthlyWage2 + wage_Per_Hour * 4;
-				workingHours = workingHours + 4;
+				totalWage += wage_Per_Hour * 4;
+				maxWorkHours += 4;
 				break;
 
 			case 1:
-				monthlyWage2 = monthlyWage2 + wage_Per_Hour * 8;
-				workingHours = workingHours + 8;
+				totalWage += wage_Per_Hour * 8;
+				maxWorkHours += 8;
 				break;
 			}
 		}
 
-		System.out.println("Employee wage for total working hours is " + monthlyWage2);
+		System.out.println("Employee Wage for Total Working Hours is " + totalWage);
+
 	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println();
+
+		Employee_Wage emp = new Employee_Wage();
+
+		emp.dailyEmployeeWage();
+		emp.wageDaysHours();
+
+	}
+
 }
